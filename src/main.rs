25633ecf066a2abc93b2142000209515952fcfeb
Will Beason <willbeason@gmail.com>
2022-09-14 10:49:56 -0500

    Commit to save work on failing computer.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 extern crate xml;
 
+use std::collections::LinkedList;
 use std::fs::File;
 use std::io::BufReader;
 use std::error::Error;
+use std::future::Future;
 use mongodb::{Client, options::ClientOptions};
 use mongodb::bson::doc;
 use mongodb::bson::oid::ObjectId;
 use mongodb::options::ReplaceOptions;
+use mongodb::results::UpdateResult;
 use serde::{Deserialize, Serialize};
+use serde::__private::doc::Error;
 
 use xml::reader::{EventReader, XmlEvent};
 
@@ -27,13 +31,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let client: Client = Client::with_options(client_options).expect("unable to connect to MongoDB");
 
-    // println!("Databases:");
-    // for name in client.list_database_names(None, None).await? {
-    //     println!("- {}", name);
-    // }
-    // return Ok(());
+    // pages
+    extract_pages(client).await;
 
-    // /c/"Program Files"/MongoDB/Server/6.0/bin/mongod.exe --dbpath="C:\\Users\\Will\\IdeaProjects\\wikopticon\\20220801-1"
+    // normalized
+
+    return Ok(());
+}
+
+async fn extract_pages(client: Client) -> Result<(), Box<dyn Error>> {
+// /c/"Program Files"/MongoDB/Server/6.0/bin/mongod.exe --dbpath="C:\\Users\\Will\\IdeaProjects\\wikopticon\\20220801-1"
     let wikipedia_path = "data\\20220801-01\\enwiki-20220801-pages-articles-multistream1.xml";
     // let wikipedia_date = "20220801-1";
 
@@ -43,7 +50,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let parser = EventReader::new(file);
 
     let mut field = "".to_string();
-    let mut cur_page = Article{
+    let mut cur_page = Article {
         id: None,
         title: "".to_string(),
         namespace: "".to_string(),
@@ -51,9 +58,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
         text: "".to_string(),
     };
 
+    client.database("wikipedia").collection::<Article>("pages").drop(None).await?;
+
     let pages: mongodb::Collection<Article> = client.database("wikipedia").collection("pages");
 
     let mut n = 0;
+
+
+
+    let x = vec![0];
+
+    let fs: LinkedList<&Future> = LinkedList::new();
 
     for e in parser {
         match e {
@@ -77,9 +92,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
             Ok(XmlEvent::Characters(str)) => {
                 match field.as_str() {
                     "id" => {
-                        let p: [u8;8] = str.parse::<i64>().expect("unable to parse ID").to_ne_bytes();
-                        let id = [0, 0, 0, 0, p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]];
-                        cur_page.id = Some(ObjectId::from_bytes(id) )
+                        if cur_page.id.is_none() {
+                            let p: [u8; 8] = str.parse::<i64>().expect("unable to parse ID").to_ne_bytes();
+                            let id = [0, 0, 0, 0, p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]];
+                            cur_page.id = Some(ObjectId::from_bytes(id))
+                        }
                     },
                     "title" => { cur_page.title = str.to_string() },
                     "ns" => { cur_page.namespace = str.to_string() },
@@ -99,23 +116,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         None => {},
                     }
 
-                    let id = cur_page.id.expect("required id").to_string();
-                    let title = cur_page.title.to_string();
-
-                    // cur_pages.push(cur_page);
-
-
-                    println!("{}: {}", id, title);
-
                     let mut options = ReplaceOptions::default();
                     options.upsert = Some(true);
 
-                    pages.replace_one(doc!{"id": cur_page.id},
-                                     cur_page, options).await
-                        .expect("unable to write to database");
+                    let f: Result<UpdateResult, mongodb::error::Error> = pages.replace_one(doc! {"_id": cur_page.id},
+                                      cur_page, options);
+
                     n = n+1;
-                    if n >= 20 {
-                        break
+                    if n % 100 == 0 {
+                        println!("{}", n);
                     }
 
                     cur_page = Article {
@@ -136,7 +145,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    println!("Hello, world!");
+    println!("{} Articles", n);
 
     return Ok(());
 }
